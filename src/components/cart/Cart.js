e58a71de701721313c2072602c8b98b94e5e901c
mthu <1652599@hcmut.edu.vn>
2019-05-18 19:48:19 +0700

    cart component modified

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -15,18 +15,45 @@ class Cart extends Component {
       <div className="cart">
         <div className="cart_myBag">My Bag</div>
         <div className="cart_body">
-          <p className="cart_body_header border_bot text_align_left">Product</p>
-          <p className="cart_body_header border_bot">Color</p>
-          <p className="cart_body_header border_bot">Size</p>
-          <p className="cart_body_header border_bot">Quantity</p>
-          <p className="cart_body_header border_bot text_align_right">Amount</p>
-          <p className="cart_body_header"> </p>
-          <p className="cart_body_header text_align_left">Total</p>
-          <Product />
-          <Color />
-          <p className="cart_body_size">S</p>
-          <Quantity />
-          <Amount />
+          <div className="cart_body_items">
+            <p className="cart_body_items_header border_bot text_align_left">
+              Product
+            </p>
+            <p className="cart_body_items_header border_bot">Color</p>
+            <p className="cart_body_items_header border_bot">Size</p>
+            <p className="cart_body_items_header border_bot">Quantity</p>
+            <p className="cart_body_items_header border_bot text_align_right">
+              Amount
+            </p>
+            <Product />
+            <Color />
+            <p className="cart_body_item_size">S</p>
+            <Quantity />
+            <Amount />
+          </div>
+          <div className="cart_body_total">
+            <p className="cart_body_total_header">Total</p>
+            <div className="cart_body_total_info">
+              <div className="cart_body_total_info_top">
+                <div className="cart_body_total_info_top_right">
+                  <p>{"Shipping & Handling"}</p>
+                  <p>Total product</p>
+                </div>
+                <div className="cart_body_total_info_top_left">
+                  <p>Free</p>
+                  <p>$6.969</p>
+                </div>
+              </div>
+              <div className="border_bot" />
+              <div className="cart_body_total_info_bot">
+                <p>Subtotal</p>
+                <p>$6.969</p>
+              </div>
+            </div>
+            <button type="button" className="cart_body_total_checkoutBtn">
+              Checkout
+            </button>
+          </div>
         </div>
       </div>
     );
